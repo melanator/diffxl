8a324c645aa52d12a9c41d3f20c92d200bd767c2
Anton <ashevchuk22@gmail.com>
2023-01-17 18:00:01 +1100

    Update __init__.py

--- src/diffxl/__init__.py
+++ src/diffxl/__init__.py
@@ -1,4 +1,31 @@
+import sys
 from openpyxl import load_workbook
+from colorama import Fore, Style
+
+def main():
+    if len(sys.argv) == 1:
+        print("No excel files")
+        return 0
+
+    elif len(sys.argv) == 2:
+        print("Specify the second file")
+        return 0
+
+    else:
+        first_excel = load_workbook(sys.argv[1])
+        second_excel = load_workbook(sys.argv[2])\
+        
+        if(first_excel == second_excel):
+            print("Workbooks are the same")
+            return 0
+
+        print(first_excel.sheetnames)
+        print(second_excel.sheetnames)
+        print(Fore.RED + str(set(second_excel.sheetnames) - set(first_excel.sheetnames)))
+        print(Style.RESET_ALL)
+
+class Compare:
+    pass
 
 if __name__ == "__main__":
-    print("Hello, world!")
+    main()
