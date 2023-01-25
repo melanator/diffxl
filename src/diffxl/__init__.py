71439a79633dff121de22fd2ee9db1217ff7d119
Anton <ashevchuk22@gmail.com>
2023-01-25 17:27:44 +1100

    One sheet search oprtion

--- src/diffxl/__init__.py
+++ src/diffxl/__init__.py
@@ -9,6 +9,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("file_1", help="First excel file")
     parser.add_argument("file_2", help="Second excel file")
+    parser.add_argument("-s", "--sheet", action="store", help="Sheet name")
     parser.add_argument("-v", "--verbose", help="Verbose mode", action="store_true")
     args = parser.parse_args()
 
@@ -29,45 +30,54 @@ def main():
         print("You selected the same workbooks\nExit")
         return SystemExit(2)
 
-    compare(load_workbook(file1), load_workbook(file2))
+    wb1, wb2 = load_workbook(file1), load_workbook(file2)
    
-
-def compare(wb1: Workbook, wb2: Workbook):  
     if(wb1 == wb2):
         print("Workbooks are the same")
         return 0
-    
-    # Naive traverse over all sheets
-    for sheet in wb1.sheetnames:
-        
-        ws = wb1[sheet]
-        
-        if sheet not in wb2.sheetnames:
-            print(RedString(f"Sheet {sheet} is deleted"))
-        else:
-            print("Changes for sheet:", RedString(sheet))
-            count = 0
-            for row in ws.iter_rows():
-                for cell in row:
-                    if cell.value != wb2[sheet][cell.coordinate].value:
-                        other_cell = wb2[sheet][cell.coordinate].value
-                        count += 1
-                        if cell.value and other_cell:
-                            print(YellowString(f"{cell.coordinate}:\t {cell.value} -> {other_cell}"))
-                        elif wb2[sheet][cell.coordinate].value:
-                            print(GreenString(f"{cell.coordinate}:\t {other_cell}"), "deleted")
-                        else:
-                            print(RedString(f"{cell.coordinate}:\t {cell.value}"), "added")
-                        
 
-        if count == 0:
-            print(GreenString("Sheet has no changes"))
-            print("-"*20)
-    
-    for sheet in wb2.sheetnames:
-        if sheet not in wb1.sheetnames:
-            print(GreenString(f"Sheet {sheet} is deleted"))
+    if args.sheet:
+        parse_sheet(wb1, wb2, args.sheet)
+    else:
+        # Naive traverse over all sheets
+        for sheet in wb1.sheetnames:
+            parse_sheet(wb1, wb2, sheet)
         
+        for sheet in wb2.sheetnames:
+            if sheet not in wb1.sheetnames:
+                print(GreenString(f"Sheet {sheet} is deleted"))
+
+
+def parse_sheet(wb1, wb2, sheet):
+    try:
+        ws = wb1[sheet]
+    except KeyError:
+        print("No such sheet")
+        return SystemExit(3)
+    
+    if sheet not in wb2.sheetnames:
+        print(RedString(f"Sheet {sheet} is deleted"))
+    else:
+        print("Changes for sheet:", RedString(sheet))
+        count = 0
+        for row in ws.iter_rows():
+            for cell in row:
+                if cell.value != wb2[sheet][cell.coordinate].value:
+                    other_cell = wb2[sheet][cell.coordinate].value
+                    count += 1
+                    if cell.value and other_cell:
+                        print(YellowString(f"{cell.coordinate}:\t {cell.value} -> {other_cell}"))
+                    elif wb2[sheet][cell.coordinate].value:
+                        print(GreenString(f"{cell.coordinate}:\t {other_cell}"), "deleted")
+                    else:
+                        print(RedString(f"{cell.coordinate}:\t {cell.value}"), "added")
+                    
+
+    if count == 0:
+        print(GreenString("Sheet has no changes"))
+        print("-"*20)
+
+       
 
 if __name__ == "__main__":
     main()
